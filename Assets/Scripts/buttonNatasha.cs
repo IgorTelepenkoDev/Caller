07912127467cf9d604d49a2f9bd1fb3d9c505126
IgorTelepenko <telepenko.igor3@gmail.com>
2021-03-03 08:22:56 +0200

    caller_startstart

---
+++ Assets/Scripts/buttonNatasha.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonNatasha : MonoBehaviour
+{
+
+    public GameObject pressedEvth;
+    public GameObject unpressedEvth;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        
+
+        pressedEvth.SetActive(true);
+
+        unpressedEvth.SetActive(false);
+
+    }
+}
